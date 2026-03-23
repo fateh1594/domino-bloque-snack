e6c34a417db70b4be81146700c6bf4d1f904bfcc
fateh1594 <hankmoody1839@gmail.com>
2026-03-23 13:12:23 +0100

    Create Board.js
    V23

---
+++ Board.js
@@ -0,0 +1,35 @@
+// Board.js
+import React from 'react';
+import { View, StyleSheet, Dimensions } from 'react-native';
+import Domino from './Domino';
+
+export default function Board({ board }) {
+  const { width, height } = Dimensions.get('window');
+
+  return (
+    <View style={[styles.board, { width, height: height * 0.6 }]}>
+      {board.map((item, idx) => (
+        <Domino
+          key={idx}
+          value={item.piece}
+          style={{
+            position: 'absolute',
+            left: item.x,
+            top: item.y,
+            transform: [{ rotate: `${item.rotation}deg` }],
+          }}
+        />
+      ))}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  board: {
+    backgroundColor: '#004d00', // vert foncé du plateau
+    borderColor: '#d4af37',     // doré pour les bordures
+    borderWidth: 4,
+    borderRadius: 10,
+    overflow: 'hidden',
+  },
+});
